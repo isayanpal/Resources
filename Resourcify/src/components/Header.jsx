79c42cc7a66229b0982ae11c16908c9295e5a17e
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-08-07 11:40:33 +0530

    ui fix

--- Resourcify/src/components/Header.jsx
+++ Resourcify/src/components/Header.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import { GrResources } from "react-icons/gr";
 
 const Header = () => {
   return (
-    <div className="flex flex-row sm:justify-around p-2 gap-3">
+    <div className="flex flex-row justify-between sm:justify-around px-2 gap-3">
       <Link to={"/"}>
-        <div className="cursor-pointer font-bold text-2xl text-[#1D24CA]">
-          Resourcify
+        <div className="cursor-pointer font-bold text-2xl text-[#1D24CA] flex flex-row items-center gap-1">
+          <GrResources /> Resourcify
         </div>
       </Link>
-      <div className="flex sm:flex-row flex-col gap-5 cursor-pointer font-semibold text-xl">
-        <Link to={"/web-dev"}>
-          <div className=" hover:text-[#1D24CA] hover:shadow-xl text-center">
-            Web Development
-          </div>
-        </Link>
-        <Link to={"/design"}>
-          <div className="hover:text-[#1D24CA] hover:shadow-xl">
-            UI & UX Design
-          </div>
-        </Link>
-      </div>
       <Link to={"https://github.com/isayanpal/Resources"} target="blank">
-        <div className="cursor-pointer hover:shadow-xl font-light">Github </div>
+        <div className="cursor-pointer hover:shadow-xl font-light flex flex-row items-center gap-1">
+          Github <FaGithub />
+        </div>
       </Link>
     </div>
   );
--- Resourcify/src/components/Home.jsx
+++ Resourcify/src/components/Home.jsx
@@ -1,15 +1,51 @@
 import React from "react";
 import home from "../assets/home.jpg";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="m-2 p-2 justify-center flex flex-col items-center gap-2">
-      <div className="text-3xl font-semibold cursor-default mt-10">
-        Welcome to my free Resources Page !!
-      </div>
-      <div className="font-medium">Get all needed resources at one place.</div>
-      <div className="flex justify-center sm:h-[700px] sm:w-[900px] h-[300px] w-[400px]">
+    <div className="mt-5 sm:mx-2 px-2 justify-center flex flex-col items-center gap-2 min-h-screen">
+      <h1 className="text-3xl font-semibold cursor-default text-center">
+        Welcome to Your Ultimate Free Resource Hub!
+      </h1>
+      <p className="font-medium">Get all needed resources at one place.</p>
+      <div className="flex justify-center sm:h-[450px] sm:w-[650px] h-[300px] w-[400px]">
         <img src={home} alt="" />
+      </div>
+      <div className="flex flex-col justify-center items-center mt-3 ">
+        <div className="flex flex-col sm:flex-row justify-center gap-10">
+          <Link to="/web-dev">
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ rotate: 360, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+              }}
+              className="w-[350px] h-[250px] text-center flex flex-col justify-center items-center  rounded-3xl shadow-xl p-2"
+            >
+              <h2 className="text-xl font-semibold">Web Development</h2>
+              <p>Start your Web Development journey from here.</p>
+            </motion.div>
+          </Link>
+          <Link to="/design">
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ rotate: 360, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+              }}
+              className="w-[350px] h-[250px] text-center flex flex-col justify-center items-center shadow-xl rounded-3xl p-2"
+            >
+              <h2 className="text-xl font-semibold">UI & UX Design</h2>
+              <p>Learn the UI UX principles and much more!</p>
+            </motion.div>
+          </Link>
+        </div>
       </div>
     </div>
   );
